18f9081eec9c4d0ec9b1cc22d101c1e84e200652
Muhammad Idrees Khan <82828412+muhammadidreeskhan@users.noreply.github.com>
2023-11-22 23:26:35 +0000

    Update work.js
     i have update some changes to it

--- src/components/data/work.js
+++ src/components/data/work.js
@@ -1,35 +1,63 @@
 export const WorkData = [
     {
-        company: "Advanced React" ,
-        designation: "Advanced React developer",
-        dateJoining: "2022",
-        dateEnd: "2028",
-        companyLogo:"https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~X57775KT4UJ4/CERTIFICATE_LANDING_PAGE~X57775KT4UJ4.jpeg",
-        work:"Create robust and reusable components with advanced techniques and learn different patterns to reuse common behavior Interact with a remote server and fetch and post data via an API         Seamlessly test React applications with React Testing Library Integrate commonly used React libraries to streamline your application development",
+      company: "Advanced React",
+      designation: "Advanced React developer",
+      dateJoining: "2022",
+      dateEnd: "2028",
+      companyLogo: "https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~X57775KT4UJ4/CERTIFICATE_LANDING_PAGE~X57775KT4UJ4.jpeg",
+      work: "Create robust and reusable components with advanced techniques and learn different patterns to reuse common behavior Interact with a remote server and fetch and post data via an API Seamlessly test React applications with React Testing Library Integrate commonly used React libraries to streamline your application development",
     },
     {
-        company: "React Basics" ,
-        designation: "AZ-900",
-        dateJoining: "2023",
-        dateEnd: "2025",
-        companyLogo:"https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~NPX2QMVQLSCK/CERTIFICATE_LANDING_PAGE~NPX2QMVQLSCK.jpeg",
-        work:"I have solid understanding of cloud computing principles and best practices.Familiarity with Azure services such as virtual machines, storage, and networking.."
+      company: "React Basics",
+      designation: "AZ-900",
+      dateJoining: "2023",
+      dateEnd: "2025",
+      companyLogo: "https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~NPX2QMVQLSCK/CERTIFICATE_LANDING_PAGE~NPX2QMVQLSCK.jpeg",
+      work: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet dapibus neque.",
     },
     {
-        company: "HTML and CSS in depth" ,
-        designation: "SC-200",
-        dateJoining: "2022",
-        dateEnd: "2023",
-        companyLogo:"https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~BH2DKVF6W7KD/CERTIFICATE_LANDING_PAGE~BH2DKVF6W7KD.jpeg",
-        work:"I proficient in security operations and incident response and experience with threat intelligence and security monitoring using Microsoft security technologies.I have knowledge of governance, risk, and compliance frameworks and practices."
+      company: "HTML and CSS in depth",
+      designation: "SC-200",
+      dateJoining: "2022",
+      dateEnd: "2023",
+      companyLogo: "https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~BH2DKVF6W7KD/CERTIFICATE_LANDING_PAGE~BH2DKVF6W7KD.jpeg",
+      work: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet dapibus neque.",
     },
     {
-        company: "Version Control" ,
-        designation: "AZ-104",
-        dateJoining: "2022",
-        dateEnd: "2023",
-        companyLogo:"https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~85PXFEJQCQ6J/CERTIFICATE_LANDING_PAGE~85PXFEJQCQ6J.jpeg",
-        work:"I have proficient in managing and maintaining Azure infrastructure and services also experience with Azure virtual machines, storage, networking, and security.I have knowledge of Azure identity and access management, monitoring, and governance."
-    }
-    
-]
+      company: "Version Control",
+      designation: "AZ-104",
+      dateJoining: "2022",
+      dateEnd: "2023",
+      companyLogo: "https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~85PXFEJQCQ6J/CERTIFICATE_LANDING_PAGE~85PXFEJQCQ6J.jpeg",
+      work: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet dapibus neque.",
+    },
+    // Add more entries with Lorem Ipsum text for the new certifications
+
+        {
+          company: "Programming with JavaScript",
+          designation: "Certificate Title",
+          dateJoining: "2023",
+          dateEnd: "2024",
+          companyLogo: "https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~U5F6Z3WDNKEP/CERTIFICATE_LANDING_PAGE~U5F6Z3WDNKEP.jpeg",
+          work: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet dapibus neque.",
+        },
+        {
+          company: "Introduction to Front end development",
+          designation: "Certificate Title",
+          dateJoining: "2023",
+          dateEnd: "2024",
+          companyLogo: "https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~PTU6TPKBNDR5/CERTIFICATE_LANDING_PAGE~PTU6TPKBNDR5.jpeg",
+          work: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet dapibus neque.",
+        },
+       
+        {
+          company: "Crash Course on Python",
+          designation: "Certificate Title",
+          dateJoining: "2022",
+          dateEnd: "2022",
+          companyLogo: "https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~UYTDYCQMGXNT/CERTIFICATE_LANDING_PAGE~UYTDYCQMGXNT.jpeg",
+          work: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet dapibus neque.",
+        },
+       
+      ];
+      
